4507f41003d822f443e556c0f4b2413d9440a971
NASREEN <sspashaes@gmail.com>
2024-09-08 12:42:15 +0530

    This is my first project

---
+++ testcode.sh
@@ -0,0 +1,8 @@
+ Hi
+  This is Saleem Pasha
+  Worked as Devops Engineer at Transol Systems Pvt Ltd
+  I have worked on various devops tools like git, github, jenkins,
+  Docker, terraform, kubernetes, Linux, AWS cloud.
+  Also i have worked on various AWS services like Ec2, S3, Route53, VPC,
+  Cloudwatch, Cloudtrail, Iam, ECR and ECS.
+
